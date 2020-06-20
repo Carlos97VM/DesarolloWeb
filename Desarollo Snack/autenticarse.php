1ebacf74b6c602cbbd1c4a78b5d5934f90fd6f22
Carlos97VM <48696073+Carlos97VM@users.noreply.github.com>
2020-06-20 18:53:23 -0400

    Add files via upload
    Desarollo de Programa de registros de venta Snack Torres

---
+++ Desarollo Snack/autenticarse.php
@@ -0,0 +1,19 @@
+<?php 
+session_start();
+include('conexion.php');
+$email=$_POST['txtUser'];
+$password=sha1($_POST['txtPassword']);
+	$sql="SELECT nombre,email,password,nivel FROM users WHERE email='$email' AND password='$password'" ;
+	$result=$con->query($sql);
+	if($fila=$result->fetch_assoc())
+	{
+		$_SESSION['nombre']=$fila['nombre'];
+		$_SESSION['nivel']=$fila['nivel'];
+		header("location:index.php");
+	}
+	else
+	{
+			echo '<script>alert("Email o contraseña incorrecta")</script>';
+			echo "<script>location.href='login.php'</script>";
+	}
+?>
